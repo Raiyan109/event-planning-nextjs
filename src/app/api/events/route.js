14744e97077ae5ad745f84b3b78ccf9d5d35e9c3
Raiyan109 <kraiyan109@gmail.com>
2024-12-26 12:38:44 +0600

    create event in server

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,14 +1,36 @@
 import connectToDatabase from "@/lib/mongodb";
+import { EventModel } from "@/models/event.model";
 import { NextResponse } from "next/server";
+
 
 
 export async function POST(req) {
     const { formValues } = await req.json();
     console.log(formValues, 'from POST');
 
-    return NextResponse.json({ message: "Hello World" });
+    // return NextResponse.json({ message: "Hello World" });
 
     // Connect to MongoDB
     await connectToDatabase();
+    try {
+        const event = new EventModel({
+            title: formValues.title,
+            description: formValues.description,
+            date: formValues.date,
+            time: formValues.time,
+            location: formValues.location
+        });
+
+        const eventCreated = await event.save();
+        console.log(eventCreated);
+        return NextResponse.json({
+            message: "Event saved successfully",
+            event: eventCreated
+        });
+
+    } catch (error) {
+        console.log(error);
+
+    }
 
 }
---
+++ src/models/event.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+export const EventModel = model('Plan', eventSchema)
